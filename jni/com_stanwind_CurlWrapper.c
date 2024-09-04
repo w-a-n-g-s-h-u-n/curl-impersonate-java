56ddbc5a872d05f016c37f3692efa70e35d2d030
Mars <21408788+w-a-n-g-s-h-u-n@users.noreply.github.com>
2024-09-04 23:21:08 +0800

    Update com_stanwind_CurlWrapper.c

--- jni/com_stanwind_CurlWrapper.c
+++ jni/com_stanwind_CurlWrapper.c
@@ -2,8 +2,7 @@
 #include "include/curl/curl.h"
 #include "com_stanwind_CurlWrapper.h"
 
-JNIEXPORT jstring JNICALL Java_com_stanwind_CurlWrapper_curlVersion
-  (JNIEnv *env, jobject) {
+JNIEXPORT jstring JNICALL Java_com_stanwind_CurlWrapper_curlVersion(JNIEnv *env, jobject) {
    const char *version = curl_version();
    return (*env)->NewStringUTF(env, version);
 }
